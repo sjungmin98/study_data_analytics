e96172b4ee7e9731487e62acaa3d13e9648f16ec
sjungmin98 <shc980@gmail.com>
2024-01-30 15:18:42 +0900

    111

--- docs/quests/DDA/SpineSurgeryList_apply.py
+++ docs/quests/DDA/SpineSurgeryList_apply.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np  # 누락값(NaN)을 처리하기 위해 numpy사용
 
 # CSV 파일 불러오기
 df = pd.read_csv('datasets/SpineSurgeryList.csv')
@@ -10,27 +11,20 @@ df['체중'] = df['체중'].apply(lambda x: x)
 # BMI 컬럼 생성
 df['BMI'] = df['체중'] / df['신장']
 
-def surgery_time(x):
-    try:    
-        hour = int(x/60)
-        min = int(x%60)
-        if int(x/60) == 0:
-            time = pd.to_datetime(f"{min}", format="%M")
-            pass
-        else:
-            time = pd.to_datetime(f"{hour}, {min}", format="%H, %M")
-            pass
-        return time
-    except:
-        pass
-pass
-df['수술시간_datetime'] = df["수술시간"].apply(lambda x: pd.to_datetime(f"{int(x/60)}, {int(x%60)}", format="%H, %M") if int(x/60) != 0 else pd.to_datetime(f"{int(x%60)}", format="%M"))
-df['수술시간_datetime'] = df['수술시간_datetime'].dt.time
-pass
+# 분을 시간과 분 형태의 문자열로 변환하고, 이를 datetime 형식으로 변환하는 함수
+def surgery_time(minutes):
+    if pd.isnull(minutes):
+        return np.nan
+    else:
+        hours = minutes // 60
+        minutes = minutes % 60
+        time_str = f"{int(hours)}:{int(minutes)}"
+        return pd.to_datetime(time_str, format='%H:%M').time()
 
+# apply 함수를 사용하여 '수술시간' 컬럼의 각 요소에 대해 함수 적용
+df['수술시간_datetime'] = df['수술시간'].apply(surgery_time)
 
-
+print(df['수술시간_datetime'])
 print(df['BMI'])
-df.info()
 pass
 
