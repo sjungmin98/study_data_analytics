9a0bc050535a075a55869fba190b36b066448c02
sjungmin98 <shc980@gmail.com>
2024-01-30 14:42:57 +0900

    11

--- docs/quests/DDA/SpineSurgeryList_apply.py
+++ docs/quests/DDA/SpineSurgeryList_apply.py
@@ -10,6 +10,27 @@ df['체중'] = df['체중'].apply(lambda x: x)
 # BMI 컬럼 생성
 df['BMI'] = df['체중'] / df['신장']
 
+def surgery_time(x):
+    try:    
+        hour = int(x/60)
+        min = int(x%60)
+        if int(x/60) == 0:
+            time = pd.to_datetime(f"{min}", format="%M")
+            pass
+        else:
+            time = pd.to_datetime(f"{hour}, {min}", format="%H, %M")
+            pass
+        return time
+    except:
+        pass
+pass
+df['수술시간_datetime'] = df["수술시간"].apply(surgery_time_time)
+df['수술시간_datetime'] = df['수술시간_datetime'].dt.time
+pass
+
+
+
 print(df['BMI'])
 df.info()
 pass
+
